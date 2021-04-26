b4e23dfd198db62bb925ffdf6763d4f30a525fa4
Perpety <berniceanne3@gmail.com>
2021-04-26 22:47:40 +0000

    Changes to layout App

---
+++ login_new_sample/android/app/src/main/kotlin/com/example/login_new_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.login_new_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ login_new_sample/lib/main.dart
@@ -0,0 +1,304 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PageApp());
+}
+
+class PageApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.yellow,
+          title: Text(
+            'PageApp',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+            child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 200,
+                      width: 300,
+                      decoration: BoxDecoration(color: Colors.blue),
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.all(20),
+                      child: Text(
+                        'This is our firt trial of PageApp',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.white,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 200,
+                      width: 300,
+                      decoration: BoxDecoration(color: Colors.blue),
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.all(20),
+                      child: Text(
+                        'This is our firt trial of PageApp',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.white,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 200,
+                      width: 300,
+                      decoration: BoxDecoration(color: Colors.blue),
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.all(20),
+                      child: Text(
+                        'This is our firt trial of PageApp',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.white,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 200,
+                      width: 300,
+                      decoration: BoxDecoration(color: Colors.blue),
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.all(20),
+                      child: Text(
+                        'This is our firt trial of PageApp',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.white,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 200,
+                      width: 300,
+                      decoration: BoxDecoration(color: Colors.blue),
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.all(20),
+                      child: Text(
+                        'This is our firt trial of PageApp',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.white,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 300,
+                    decoration: BoxDecoration(color: Colors.blue),
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                      'This is our firt trial of PageApp',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 300,
+                    decoration: BoxDecoration(color: Colors.blue),
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                      'This is our firt trial of PageApp',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 300,
+                    decoration: BoxDecoration(color: Colors.blue),
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                      'This is our firt trial of PageApp',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 300,
+                    decoration: BoxDecoration(color: Colors.blue),
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                      'This is our firt trial of PageApp',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 300,
+                    decoration: BoxDecoration(color: Colors.blue),
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                      'This is our firt trial of PageApp',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 300,
+                    decoration: BoxDecoration(color: Colors.blue),
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                      'This is our firt trial of PageApp',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 300,
+                    decoration: BoxDecoration(color: Colors.blue),
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                      'This is our firt trial of PageApp',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 300,
+                    decoration: BoxDecoration(color: Colors.blue),
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                      'This is our firt trial of PageApp',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
